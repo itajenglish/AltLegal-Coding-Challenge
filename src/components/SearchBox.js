228aee0b9c63d403fce52a606b28394fb73ca543
Taj English <tajenglish@gmail.com>
2017-02-18 11:43:04 -0500

    Added Event Listeners to searchbox for State Changes

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,18 +1,26 @@
-import React, { Component } from 'react';
-import { valHash } from '../../helpers/validateHashTag'
+import React, {Component} from 'react';
+import {valHash} from '../../helpers/validateHashTag'
 
 class SearchBox extends Component {
 
-  handleChange(event){
-    console.log(event.target.value);
+  handleFocus(event) {
+    event.target.placeholder = '';
+  }
+
+  handleBlur(event) {
+    event.target.placeholder = 'Add a hashtag'
+  }
+
+  handleChange(event) {
+    console.log(event);
     console.log(valHash(event.target.value));
   }
 
   render() {
     return (
-      <div id="searchBoxContainer" >
+      <div id="searchBoxContainer">
         <div className="panel-body">
-          <input id="searchBox" onChange={(event) => this.handleChange(event)} type="text" style={null} placeholder="Add a hashtag"/>
+          <input id="searchBox" placeholder="Add a hashtag" type="text" onBlur={(event) => this.handleBlur(event)} onFocus={(event) => this.handleFocus(event)} onChange={(event) => this.handleChange(event)} />
         </div>
       </div>
     )
