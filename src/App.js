e69de1911e84b24efe5e85bee2c3bc7330fe5263
Taj English <tajenglish@gmail.com>
2017-02-18 21:56:04 -0500

    Card State Working

--- src/App.js
+++ src/App.js
@@ -1,31 +1,68 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Header from './components/Header';
-import SearchBox from './components/SearchBox';
 import Card from './components/Card';
-import './App.css';
 import Api from '../utils/Api';
+import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      card_one_active: false,
+      card_two_active: false,
+      card_three_active: false
+    }
+  }
+
+  componentWillMount() {
+    //Gets Number Of Watchlists and sets number to activeCardNumber in state.
+    Api.getAllWatchlist().then((response) => {
+      this.setState({activeCardNumber: response.data.length});
+
+      //Set Card Active State Based On Array Length.
+    switch (response.data.length) {
+  case 1:
+    this.setState({card_one_active: true});
+    console.log(`card: 1 Active!`)
+    break;
+  case 2:
+    this.setState({card_one_active: true, card_two_active: true});
+    console.log(`card: 2 Active!`)
+    break;
+  case 3:
+    this.setState({card_one_active: true, card_two_active: true, card_three_active: true});
+    console.log(`card: 2 Active!`)
+    break;
+  default:
+    return
+}
+
+      console.log(response);
+    }).catch((err) => {
+      console.log(err);
+    })
+
+  }
 
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header/>
 
         <div className="container">
           <div className="row">
 
             <div className="col-md-4">
-              <SearchBox />
-              <Card />
+              <Card id={1} cardState={this.state.card_one_active}/>
             </div>
 
             <div className="col-md-4">
-              <Card />
+              <Card id={2} cardState={this.state.card_two_active}/>
             </div>
 
             <div className="col-md-4">
-              <Card />
+              <Card id={3} cardState={this.state.card_three_active}/>
             </div>
 
           </div>
--- src/components/card.js
+++ src/components/card.js
@@ -1,17 +1,50 @@
 import React, { Component } from 'react';
+import Api from '../../utils/Api';
 
 class Card extends Component {
+  constructor(){
+    super();
+    this.state = {
+      activeCardNumber: null,
+      tweets: null
+    }
+  }
+
+  componentWillMount() {
+    //Gets Number Of Watchlists and sets number to activeCardNumber in state.
+    Api.getAllWatchlist()
+    .then((watchlist) => {
+      this.setState({activeCardNumber: watchlist.data.length})
+
+    console.log(watchlist);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+}
+
+
+  renderCard(){
+    if (this.props.cardState === true){
+      return(
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <p>Place Holder</p>
+          </div>
+          <div className="panel-body">
+            Panel content
+          </div>
+        </div>
+      )
+    } else {
+      return null
+    }
+  }
+
+
+
   render() {
-    return (
-      <div className="panel panel-default">
-        <div className="panel-heading">
-          <p>Place Holder</p>
-        </div>
-        <div className="panel-body">
-          Panel content
-        </div>
-      </div>
-    )
+    return (this.renderCard())
   }
 }
 
