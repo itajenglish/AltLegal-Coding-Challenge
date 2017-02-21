4b481653672fdcdef517d1decf841f9bc5a39596
Taj English <tajenglish@gmail.com>
2017-02-20 23:49:27 -0500

    Prepare for deploy

--- utils/Api.js
+++ utils/Api.js
@@ -2,22 +2,22 @@ const axios = require('axios');
 
 module.exports = {
   getAllWatchlist: () => {
-    return axios.get('http://localhost:8080/api/watchlist')
+    return axios.get('https://alt-legal-api.herokuapp.com/api/watchlist')
   },
   addNewWatchlist: (data) => {
-    return axios.post('http://localhost:8080/api/watchlist', data)
+    return axios.post('https://alt-legal-api.herokuapp.com/api/watchlist', data)
   },
   getOneWatchlist: (card_id) => {
-    return axios.get(`http://localhost:8080/api/watchlist/${card_id}`)
+    return axios.get(`https://alt-legal-api.herokuapp.com/api/watchlist/${card_id}`)
   },
   updateWatchlist: (card_id, hashtag) =>{
-    return axios.put(`http://localhost:8080/api/watchlist/${card_id}`, hashtag)
+    return axios.put(`https://alt-legal-api.herokuapp.com/api/watchlist/${card_id}`, hashtag)
   },
   deleteWatchlist: (card_id) => {
-    return axios.delete(`http://localhost:8080/api/watchlist/${card_id}`)
+    return axios.delete(`https://alt-legal-api.herokuapp.com/api/watchlist/${card_id}`)
   },
   getTweets: (hashtag) => {
-    return axios.get(`http://localhost:8080/api/tweets/${hashtag}`)
+    return axios.get(`https://alt-legal-api.herokuapp.com/api/tweets/${hashtag}`)
   }
 
 }
