99f1e72a8241f8fb8b78d891dc3228f95dfd6ae0
Taj English <tajenglish@gmail.com>
2017-03-08 21:43:27 -0500

    Fixed wrong Api URL

--- utils/Api.js
+++ utils/Api.js
@@ -2,21 +2,21 @@ const axios = require('axios');
 
 module.exports = {
   getAllWatchlist: () => {
-    return axios.get('https://twitt-watch.herokuapp.com/api/watchlist')
+    return axios.get('https://twitt-watch-api.herokuapp.com/api/watchlist')
   },
   addNewWatchlist: (data) => {
-    return axios.post('https://twitt-watch.herokuapp.com/api/watchlist', data)
+    return axios.post('https://twitt-watch-api.herokuapp.com/api/watchlist', data)
   },
   getOneWatchlist: (card_id) => {
-    return axios.get(`https://twitt-watch.herokuapp.com/api/watchlist/${card_id}`)
+    return axios.get(`https://twitt-watch-api.herokuapp.com/api/watchlist/${card_id}`)
   },
   updateWatchlist: (card_id, hashtag) => {
-    return axios.put(`https://twitt-watch.herokuapp.com/api/watchlist/${card_id}`, hashtag)
+    return axios.put(`https://twitt-watch-api.herokuapp.com/api/watchlist/${card_id}`, hashtag)
   },
   deleteWatchlist: (card_id) => {
-    return axios.delete(`https://twitt-watch.herokuapp.com/api/watchlist/${card_id}`)
+    return axios.delete(`https://twitt-watch-api.herokuapp.com/api/watchlist/${card_id}`)
   },
   getTweets: (hashtag) => {
-    return axios.get(`https://twitt-watch.herokuapp.com/api/tweets/${hashtag}`)
+    return axios.get(`https://twitt-watch-api.herokuapp.com/api/tweets/${hashtag}`)
   }
 }
