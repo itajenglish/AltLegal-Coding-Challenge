aac6499f88428228443cc5d1f030e63249d208f8
Taj English <tajenglish@gmail.com>
2017-03-08 21:55:59 -0500

    Restoring localhost endpoints

--- utils/Api.js
+++ utils/Api.js
@@ -2,21 +2,21 @@ const axios = require('axios');
 
 module.exports = {
   getAllWatchlist: () => {
-    return axios.get('https://twitt-watch-api.herokuapp.com/api/watchlist')
+    return axios.get('http://localhost:8080/api/watchlist')
   },
   addNewWatchlist: (data) => {
-    return axios.post('https://twitt-watch-api.herokuapp.com/api/watchlist', data)
+    return axios.post('http://localhost:8080/api/watchlist', data)
   },
   getOneWatchlist: (card_id) => {
-    return axios.get(`https://twitt-watch-api.herokuapp.com/api/watchlist/${card_id}`)
+    return axios.get(`http://localhost:8080/api/watchlist/${card_id}`)
   },
   updateWatchlist: (card_id, hashtag) => {
-    return axios.put(`https://twitt-watch-api.herokuapp.com/api/watchlist/${card_id}`, hashtag)
+    return axios.put(`http://localhost:8080/api/watchlist/${card_id}`, hashtag)
   },
   deleteWatchlist: (card_id) => {
-    return axios.delete(`https://twitt-watch-api.herokuapp.com/api/watchlist/${card_id}`)
+    return axios.delete(`http://localhost:8080/api/watchlist/${card_id}`)
   },
   getTweets: (hashtag) => {
-    return axios.get(`https://twitt-watch-api.herokuapp.com/api/tweets/${hashtag}`)
+    return axios.get(`http://localhost:8080/api/tweets/${hashtag}`)
   }
 }
